1190a5f6cba655c888be8c8120ea55b36064de39
GerardoMac <centrodph@gmail.com>
2018-06-12 11:07:46 -0300

    slide mobile

--- index.php
+++ index.php
@@ -6,37 +6,7 @@
     <?php include("./de/meta.php");
 ?>
     <script type="text/javascript">
-        $j(document).ready(function () {
-            // var size = $j(window).width();
-            // if (size  1000) return;
-            // $j('#slider').nivoSlider({
-            //     effect: 'fade', // Specify sets like: 'fold,fade,sliceDown'
-            //     slices: 15, // For slice animations
-            //     boxCols: 8, // For box animations
-            //     boxRows: 4, // For box animations
-            //     animSpeed: 900, // Slide transition speed
-            //     pauseTime: 5000, // How long each slide will show
-            //     startSlide: 0, // Set starting Slide (0 index)
-            //     directionNav: false, // Next & Prev navigation
-            //     directionNavHide: false, // Only show on hover
-            //     controlNav: false, // 1,2,3... navigation
-            //     controlNavThumbs: false, // Use thumbnails for Control Nav
-            //     controlNavThumbsFromRel: false, // Use image rel for thumbs
-            //     controlNavThumbsSearch: '.jpg', // Replace this with...
-            //     controlNavThumbsReplace: '_thumb.jpg', // ...this in thumb Image src
-            //     keyboardNav: true, // Use left & right arrows
-            //     pauseOnHover: false, // Stop animation while hovering
-            //     manualAdvance: false, // Force manual transitions
-            //     captionOpacity: 0.8, // Universal caption opacity
-            //     prevText: 'Prev', // Prev directionNav text
-            //     nextText: 'Next', // Next directionNav text
-            //     beforeChange: function () { }, // Triggers before a slide transition
-            //     afterChange: function () { }, // Triggers after a slide transition
-            //     slideshowEnd: function () { }, // Triggers after all slides have been shown
-            //     lastSlide: function () { }, // Triggers when last slide is shown
-            //     afterLoad: function () { } // Triggers when slider has loaded
-            // });
-        });
+        $j(document).ready(function () {        
         Shadowbox.init();
     </script>
     <link rel="stylesheet" type="text/css" href="sh/shadowbox.css">
@@ -70,7 +40,13 @@
                             style="display:none;">uYeDkoGk4a4</a>
                         <a href="https://www.youtube.com/embed/OCKEzsXER-k?rel=0&autoplay=1" class="blue linkbig" title="Video 5 de 5" rel="shadowbox[Videos];height=315;width=560"
                             style="display:none;">OCKEzsXER-k</a>
-                        <img src="./de/images/homeMobile.jpg" class="only-mobile" />
+                        <div class="slidermobile">
+                            <div id="sliderImages">
+                            <img src="./de/images/homeMobile.jpg" class="only-mobile" />
+                            <img src="./de/images/homeMobile1.jpg" class="only-mobile" />
+                            <img src="./de/images/homeMobile2.jpg" class="only-mobile" />
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <div class="homeBottom">
